c3752e1ca347099d649dd1fbb5979362353f8e35
partizand <partizand@gmail.com>
2011-10-04 15:49:03 +0400

    Начало функции InRange

--- filterprop.pas
+++ filterprop.pas
@@ -43,9 +43,11 @@ type
 type
    TFiltProp=class
    public
-     constructor TFiltProp;
+     constructor TFiltProp(FilterType:TFilterType;RootDir:string);
+     function IsDirInRange(DirName:string):boolean; // Попадает ли каталог в фильтр
      procedure Clear; // Очистить
      function IsEmpty:boolean; // Фильтр пуст
+     function GetType:TFilterType; // Возвращает тип фильтра
      procedure AddDir(DirName:string); // Добавить каталог, относительно источника
      procedure RemoveDir(DirName:string);
      procedure RemoveDir(Index:integer);
@@ -59,15 +61,24 @@ type
      procedure SaveToFile(XMLConf:TXMLConfig;Section:string);
    public
 
-     Dirs:TStringList;
-     Files:TStringList;
-     Masks:TStringList;
+     //property Dirs:TStringList read GetDirs;
+     //Dirs:TStringList;
+     //Files:TStringList;
+     //Masks:TStringList;
    private
      procedure RemoveByIndex(List:TStringList;Index:integer); // Удалить по индексу
      procedure RemoveByName(List:TStringList;EntryName:string); // Удалить по имени
+     function GetEmpty:boolean; // Высчитывает _isEmpty, возвращает его значение
+
+
    private
+     Dirs:TStringList;
+     Files:TStringList;
+     Masks:TStringList;
+     _RootDir:string; // Корневой каталог для фильтра
      _isEmpty:boolean; // Кэшированное значение IsEmpty
      _isChanged:boolean; // Фильтр менялся, Если false то кэшированное значение верно
+     _FilterType:TFilterType; // Тип фильтра
    end;
 
 
@@ -75,6 +86,64 @@ type
 
 
 implementation
+//------------------------------------------------------------------------------
+// Попадает ли каталог в фильтр
+function IsDirInRange(DirName:string):boolean;
+var
+ i:integer;
+ fullPath,uDirName:string;
+begin
+// Перебираем все каталоги, ищем совпадения
+uDirName:=TCustomFS.ToUnixSep(DirName);
+for i:=1 to Dirs.Count do
+ begin
+ fullPath:=TCustomFS.PathCombine(_RootDir,Dirs[i]);
+ Same;
+ end;
+end;
+
+//------------------------------------------------------------------------------
+constructor TFiltProp.TFiltProp(FilterType:TFilterType;RootDir:string);
+begin
+_isEmpty:=true;
+_isChanged:=false;
+_FilterType:=FilterType;
+_RootDir:=TCustomFS.ToUnixSep(RootDir);
+end;
+//------------------------------------------------------------------------------
+function TFiltProp.GetType:TFilterType; // Возвращает тип фильтра
+begin
+Result:=_FilterType;
+end;
+
+//------------------------------------------------------------------------------
+// Фильтр пуст
+function TFiltProp.IsEmpty:boolean;
+begin
+  if _isChanged=false then
+     begin
+     Result:=_isEmpty
+
+     end
+    else
+     begin
+     Result:=GetEmpty;
+     end;
+end;
+//------------------------------------------------------------------------------
+// Высчитывает _isEmpty, возвращает его значение
+function TFiltProp.GetEmpty:boolean;
+var
+ i:integer;
+begin
+  i:=Dirs.Count+Files.Count+Masks.Count;
+  if i=0 then
+      _isEmpty:=true
+    else
+      _isEmpty:=false;
+  Result:=_isEmpty;
+  _isChanged:=false;
+end;
 
 //------------------------------------------------------------------------------
 // Записать в файл
@@ -139,7 +208,7 @@ begin
    str:=XMLConf.GetValue(sec + 'value','');
    AddMask(str);
   end;
-
+_isChanged:=true;
 end;
 
 //------------------------------------------------------------------------------
@@ -149,6 +218,8 @@ begin
 Dirs.Clear;
 Files.Clear;
 Masks.Clear;
+_isChanged:=false;
+_isEmpty:=true;;
 end;
 //------------------------------------------------------------------------------
 // Добавить каталог
@@ -159,6 +230,7 @@ begin
   if DirName='' then exit;
   strtmp:=TCustomFS.ToUnixSep(DirName);
   Dirs.Add(strtmp);
+  _isChanged:=true;
 end;
 //------------------------------------------------------------------------------
 // Добавить файл
@@ -169,6 +241,7 @@ begin
   if FileName='' then exit;
   strtmp:=TCustomFS.ToUnixSep(FileName);
   Files.Add(strtmp);
+  _isChanged:=true;
 end;
 //------------------------------------------------------------------------------
 // Добавить маску
@@ -176,6 +249,7 @@ procedure TFiltProp.AddMask(MaskName:string);
 begin
   if MaskName='' then exit;
   Masks.Add(MaskName);
+  _isChanged:=true;
 end;
 //------------------------------------------------------------------------------
 // Удалить каталог по имени
@@ -230,6 +304,7 @@ procedure TFiltProp.RemoveByIndex(List:TStringList;Index:integer);
 begin
   if Index>List.Count then exit;
   List.Delete(Index);
+  _isChanged:=true;
 end;
 end.
 
