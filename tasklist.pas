41b0efd528de6769fa84a623aae4bcb1058b9deb
Andrey Kapustin <partizand@gmail.com>
2011-10-09 22:12:41 +0400

    Запись/чтение TaskList из файла

--- tasklist.pas
+++ tasklist.pas
@@ -10,7 +10,7 @@ unit tasklist;
 interface
 
 uses
-  Classes, SysUtils,task,MsgStrings;
+  Classes, SysUtils,XMLCfg,task,MsgStrings;
 
 const
   MaxTasks = 100; // Макс количество заданий
@@ -29,6 +29,8 @@ type
   public
      constructor Create;
      destructor Destroy; override;
+     procedure LoadFromFile(XMLDoc:TXMLConfig;Section:string);
+     procedure SaveToFile(XMLDoc:TXMLConfig;Section:string);
      // Очистить список заданий
      procedure Clear;
      // Удалить задание
@@ -61,6 +63,47 @@ FTasks.Create;
 //_Count:=0;
 end;
 //------------------------------------------------------------------------------
+procedure TTaskList.LoadFromFile(XMLDoc:TXMLConfig;Section:string);
+var
+  i, cnt: integer;
+  sec: string;
+  Task:TTask;
+  //cr:string;
+begin
+  Task:=TTask.Create;
+  // количество заданий
+  cnt := xmldoc.GetValue(Section+'tasks/count/value', 0);
+  for i := 0 to cnt-1 do
+  begin
+    // Имя секции с заданием
+    sec := Section+'tasks/task' + IntToStr(i+1) + '/';
+    Task.LoadFromFile(XMLDoc,sec);
+    Add(Task); // Добавление копированием
+  end;
+
+end;
+//------------------------------------------------------------------------------
+procedure TTaskList.SaveToFile(XMLDoc:TXMLConfig;Section:string);
+var
+  i, cnt: integer;
+  sec: string;
+  PTask:PTTask;
+  //cr:string;
+begin
+  // количество заданий
+  cnt := xmldoc.GetValue(Section+'tasks/count/value', 0);
+  // количество заданий
+  xmldoc.SetValue(Section+'tasks/count/value', FTasks.Count);
+  for i := 0 to FTasks.Count-1 do
+  begin
+    // Имя секции с заданием
+    sec := Section+'tasks/task' + IntToStr(i+1) + '/';
+    PTask:=FTasks[i];
+    PTask^.SaveToFile(XMLDoc,sec);
+  end;
+
+end;
+//------------------------------------------------------------------------------
 // Удалить задание
 procedure TTaskList.Delete(Index:integer);
 var
@@ -71,7 +114,7 @@ begin
   FTasks.Delete(Index);
 end;
 //------------------------------------------------------------------------------
-// Добавить задание
+// Добавить задание копированием
 function TTaskList.Add(Task:TTask):integer;
 var
   tmpTask:TTask;
